6b864c4ae24c6c9a893ffae0a2c17e7846f561cd
JMakoti <josephmwamuye11@gmail.com>
2025-06-11 09:58:40 +0300

    add contact data.dart & map the data to the list view

---
+++ lib/data/contact.dart
@@ -0,0 +1,13 @@
+
+class Contact {
+  final String name;
+  final String email;
+  final String phoneNumber;
+
+  //constructor with optional paramenter
+  Contact({
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+  });
+}
--- lib/ui/contact_list/contact_list_page.dart
+++ lib/ui/contact_list/contact_list_page.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
-// import 'package:faker/faker.dart' as faker;
 import 'package:faker/faker.dart';
+import 'package:contact_app/data/contact.dart';
 
 class ContactListPage extends StatelessWidget {
-  const ContactListPage({super.key});
+ //underscore is used to indicate that this variable is private
+  final List<Contact> _contacts = List.generate( 50, (index){
+    return Contact(
+      name: faker.person.name(),
+      email: faker.internet.email(),
+      phoneNumber: faker.randomGenerator.integer(
+        1000000000, // Minimum 10-digit number
+      ).toString(),
+
+    );
+    });
+
+  ContactListPage({super.key});
+  // const ContactListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Contacts')),
       body: ListView.builder(
-        itemCount: 30,
+        itemCount: _contacts.length, // Sets the number of items in the list
         // Runs the builder function for each item in the list
         itemBuilder: (context, index) {
-          return Center(
-            child: Text(
-              faker.person.name(), // Generates a random name
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
+          return ListTile(
+            title: Text(_contacts[index].name), // Displays the contact's name
+            subtitle: Text(_contacts[index].email), // Displays the contact's email
           );
         },
       ),
